54a9ea5dd36a2c29fc01af80cdab30ea17f742e1
IdmFoundInHim <idmfoundinhim@gmail.com>
2022-04-21 00:41:57 -0400

    Improve GUI

--- gui.lisp
+++ gui.lisp
@@ -10,16 +10,24 @@ mnemonic (top-level container)
 (in-package :idmfoundinhim.mnemonic-gui)
 (in-readtable :qtools)
 
+(defparameter *font-size* 24)
+
 (define-widget mnemonic (QWidget)
   ())
 
+(define-initializer (mnemonic frameup)
+  (setf (q+:window-title mnemonic) "Mnemonic")
+  (setf (q+:fixed-size mnemonic) (q+:make-qsize 1400 800)))
+
 (define-subwidget (mnemonic input) (q+:make-qtextedit mnemonic)
-  )
+  (setf (q+:font-point-size input) *font-size*))
 
 (define-subwidget (mnemonic output) (q+:make-qtextedit mnemonic)
+  (setf (q+:font-point-size output) *font-size*)
   (setf (q+:read-only output) t))
 
 (define-subwidget (mnemonic layout) (q+:make-qvboxlayout mnemonic)
+  (setf (q+:spacing layout) 0)
   (q+:add-widget layout input)
   (q+:add-widget layout output))
 
